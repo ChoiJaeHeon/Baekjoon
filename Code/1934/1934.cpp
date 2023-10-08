e29e1b58d9e01b590d74224c6c1a56b3b91f635e
ChoiJaeHeon <jaeheon0520@naver.com>
2023-10-08 18:53:57 +0900

    최소공배수

---
+++ Code/1934/1934.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int GCD(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return (GCD(b, a % b));
+}
+
+int main () {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int num, a, b, max;
+    cin >> num;
+
+    for (int i = 0; i < num; i ++) {
+        cin >> a >> b;
+        max = GCD(a,b);
+        cout << a * b / max << '\n';
+    }
+    return 0;
+}
