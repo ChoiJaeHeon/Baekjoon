1e7913d5d2787fb63df056c91890ed0701150276
ChoiJaeHeon <jaeheon0520@naver.com>
2023-11-25 15:21:57 +0900

    15650: N과 M(2)

--- Code/15650/15650.cpp
+++ Code/15650/15650.cpp
@@ -15,7 +15,7 @@ void dfs(int depth, int cnt) {
         return;
     }
     else {
-        for (int i = depth; i <= n; i++) {
+        for (int i = depth; i <= n; i++) { // depth를 1로 전달받아서 i는 1부터 시작
             if (!visited[i]) {
                 visited[i] = true;
                 arr[cnt] = i;
@@ -29,6 +29,6 @@ void dfs(int depth, int cnt) {
 int main() {
 
     cin >> n >> m;
-    dfs(1, 0);
+    dfs(1, 0); // cnt == 시작점
     return 0;
 }
