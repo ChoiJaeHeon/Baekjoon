aa5be3675b4360ee21ef0ad237cbbb3772aa5c5a
ChoiJaeHeon <jaeheon0520@naver.com>
2023-12-01 16:44:36 +0900

    15652: N과 M(4)

--- Code/15652/15652.cpp
+++ Code/15652/15652.cpp
@@ -17,7 +17,7 @@ void dfs(int num, int depth) {
     else {
         for (int i = num; i <= n; i++) {
             arr[depth] = i;
-            dfs(i, depth + 1);
+            dfs(i, depth + 1); // num 을 쓰면 안됨, num 은 반복문을 한번 돌아도 변하지 않으니까 앞의 숫자 이하의 값이 계속 고려됨
         }
     }
 }
