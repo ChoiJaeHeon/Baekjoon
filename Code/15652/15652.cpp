94649c59b75e2659d68dcaf3cfa22e8d6133027b
ChoiJaeHeon <jaeheon0520@naver.com>
2023-12-01 16:42:45 +0900

    15652: N과 M(4)

---
+++ Code/15652/15652.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define MAX 9
+using namespace std;
+
+int n, m;
+int arr[MAX];
+bool visited[MAX];
+
+void dfs(int num, int depth) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    else {
+        for (int i = num; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    dfs(1, 0);
+    return 0;
+}
