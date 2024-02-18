034f0dbd67f3805a6fb0b554138d0820ebf1fe43
ChoiJaeHeon <jaeheon0520@naver.com>
2024-02-18 22:23:26 +0900

    1292: 쉽게 푸는 문제

---
+++ Code/1292/1292.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+start, end = map(int, input().split())
+
+arr = [0]
+
+for i in range (46):
+    for j in range (i):
+        arr.append(i)
+    
+sum_ = 0
+    
+for i in range (start, end +1):
+    sum_ += arr[i]
+    
+print(sum_)
